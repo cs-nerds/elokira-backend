06645b409bcb49c0580442ffbc5db8de959e4378
Lemmah <lemayiannakolah@gmail.com>
2021-02-22 04:47:57 +0300

    refactor: use json for all api responses

--- src/main/kotlin/web/IndexResource.kt
+++ src/main/kotlin/web/IndexResource.kt
@@ -1,16 +1,17 @@
 package web
 
 import io.ktor.application.call
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
+import io.ktor.http.*
+import io.ktor.response.*
 import io.ktor.routing.Route
 import io.ktor.routing.get
 
 fun Route.index() {
 
-    val indexPage = javaClass.getResource("/index.html").readText()
-
     get("/") {
-        call.respondText(indexPage, ContentType.Text.Html)
+        call.respond(
+            HttpStatusCode.OK,
+            mapOf("status" to "It works!", "message" to "Welcome to Elokira Vote API")
+        )
     }
 }
